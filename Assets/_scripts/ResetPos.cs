37d83d1c00fa4eef79672a80e9fe3513590b8c94
Faraz Khalid <farazkhalid05@yahoo.com>
2020-04-25 21:26:03 +0500

    first commit

---
+++ Assets/_scripts/ResetPos.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPos : MonoBehaviour
+{
+
+    Vector3 originalpos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalpos = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "SHIPWALL")
+        {
+            transform.localPosition = originalpos;
+        }
+
+        else if (other.gameObject.tag == "PLANEWALL")
+        {
+            transform.localPosition = originalpos;
+        }
+
+        else if (other.gameObject.tag == "CLOUDWALL")
+        {
+            transform.localPosition = originalpos;
+        }
+    }
+}
